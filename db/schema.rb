8685045cbf7c7fef6a78a1913afc38821621a4e8
Mario Ballesteros <mariob360@gmail.com>
2017-02-23 06:27:39 +0000

    added contact create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222231644) do
+ActiveRecord::Schema.define(version: 20170223061447) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.text     "commments"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
