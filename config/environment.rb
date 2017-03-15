0d4a26736835e63667ae0909ca2c25070a3dcdec
Mario Ballesteros <mariob360@gmail.com>
2017-03-15 00:25:22 +0000

    changes to user/pass

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require_relative 'application'
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'your_sendgrid_username',
-  :password => 'your_sendgrid_password',
+  :user_name => 'SENDGRID_USERNAME',
+  :password => 'SENDGRID_PASSWORD',
   :domain => 'young-ocean-19130.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
