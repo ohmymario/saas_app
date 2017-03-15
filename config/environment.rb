8f9766cc52776088206b8a8633be2ed0ac15d6a5
Mario Ballesteros <mariob360@gmail.com>
2017-03-15 00:04:43 +0000

    attempted fix for post

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'young-ocean-19130.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
 
 
 # Initialize the Rails application.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
     get '/about', to: 'pages#about' 
-    resources :contacts, only: [:create]
+    resources :contacts, only: :create
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
     # get request for "contact-us" webpage then goes to contacts *controller *new action
 end
