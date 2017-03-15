eca664f8c72b242c97e7ce1a1f0db5b8898a6cda
Mario Ballesteros <mariob360@gmail.com>
2017-03-15 00:20:05 +0000

    changed on enviromental

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
-
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'your_sendgrid_username',
+  :password => 'your_sendgrid_password',
+  :domain => 'young-ocean-19130.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
 # Initialize the Rails application.
 Rails.application.initialize!
