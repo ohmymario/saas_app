695a9c28c9daeed4dc4695f873f6962813c32a52
Mario Ballesteros <mariob360@gmail.com>
2017-05-25 23:58:25 +0000

    added bootstrap to registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-    root to: 'pages#home'
-    get '/about', to: 'pages#about' 
-    resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact'
-    # get request for "contact-us" webpage then goes to contacts *controller *new action
+  root to: 'pages#home'
+  get '/about', to: 'pages#about' 
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  # get request for "contact-us" webpage then goes to contacts *controller *new action
 end
